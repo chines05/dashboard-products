e6efc004567d6ac34ff8c6590649be87b8f0031c
chines05 <gpalmenara@gmail.com>
2025-06-08 10:44:25 -0300

    Feat: Adiciona integração com Prisma e CRUD para produtos

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = express();
 const PORT = 3001;
@@ -7,8 +10,22 @@ const PORT = 3001;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Backend está funcionando!");
+app.get("/products", async (req: Request, res: Response) => {
+  const allProducts = await prisma.product.findMany();
+
+  res.status(200).send(allProducts);
+});
+
+app.post("/products", async (req: Request, res: Response) => {
+  const product = await prisma.product.create({
+    data: {
+      name: req.body.name,
+      price: req.body.price,
+      description: req.body.description,
+    },
+  });
+
+  res.status(201).send(product);
 });
 
 app.listen(PORT, () => {
